62f9fa869e703a25439f4abf5e1540d4a9b986e0
katt <octokatt@gmail.com>
2019-03-03 18:47:33 -0800

    initial commit, good

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Image Class with Portland d data group',
+    author='katt',
+    license='MIT',
+)
